7adc196cbaa9ed2157b4889fbab175ab2be9af31
Gustavo Taima <87939158+taima157@users.noreply.github.com>
2022-08-24 20:51:41 -0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="Footer">
       <p>© 2022-2022 Gustavo Taima - Todos os direitos reservados.</p>
-      <p><a href="https://github.com/taima157" rel="noreferrer" target="_blank">Github.</a></p>
+      <p><a href="https://github.com/taima157/pokedex" rel="noreferrer" target="_blank">Github.</a></p>
     </div>
   )
 }
